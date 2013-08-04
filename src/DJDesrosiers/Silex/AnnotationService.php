46b41989fe8cd291da9e0a930202ed5b7bbe20fb
Dana Desrosiers <dana.desrosiers@gmail.com>
2013-08-03 18:04:46 -0700

    cached the reader and moved it to the service constructor

--- src/DJDesrosiers/Silex/AnnotationService.php
+++ src/DJDesrosiers/Silex/AnnotationService.php
@@ -9,21 +9,29 @@ class AnnotationService
 	/** @var Application */
 	protected $app;
 	
+	/** @var AnnotationReader */
+	protected $reader;
+	
 	public function __construct(Application $app)
 	{
 		$this->app = $app;
+		
+		// TODO: Allow configuration of caching service
+		$this->reader = new CachedReader(
+			new AnnotationReader(),
+			new ApcCache(),
+			$this->app['debug']
+		);
 	}
 	
 	public function registerController($controller_name)
 	{
-		// TODO: cache the reader
-		$reader = new AnnotationReader();
 		$reflection_class = new ReflectionClass($controller_name);
 		foreach ($reflection_class->getMethods(ReflectionMethod::IS_PUBLIC) as $reflection_method)
 		{
 			if (!$reflection_method->isStatic())
 			{
-				$method_annotations = $reader->getMethodAnnotations($reflection_method);
+				$method_annotations = $this->reader->getMethodAnnotations($reflection_method);
 				foreach ($method_annotations as $annotation)
 				{
 					if ($annotation instanceof Route)
