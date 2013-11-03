0113f0ba6e490d346619819c5d9e4fdfa3599988
Dana Desrosiers <dana.desrosiers@gmail.com>
2013-11-03 14:25:16 -0800

    Added Modifier Annotation that acts as a catch-all modifier.  It can be used to express any Silex modifier, even any that might be added in the future.

---
+++ src/DDesrosiers/SilexAnnotations/Annotations/Modifier.php
@@ -0,0 +1,39 @@
+<?php 
+/**
+ * This file is part of the silex-annotation-provider package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ * @copyright (c) 2013, Dana Desrosiers <dana.desrosiers@gmail.com>
+ */
+
+namespace DDesrosiers\SilexAnnotations\Annotations;
+
+/**
+ * @Annotation
+ * @Target({"METHOD", "ANNOTATION"})
+ */
+class Modifier implements RouteAnnotation
+{
+	/** @var string */
+	public $method;
+
+	/** @var array */
+	public $args;
+
+	/**
+	 * @param \Silex\Controller $controller
+	 *
+	 * @throws \RuntimeException
+	 */
+	public function process(\Silex\Controller $controller)
+	{
+		if (!method_exists($controller->getRoute(), $this->method))
+		{
+			throw new \RuntimeException("Modifier: [$this->method] does not exist.");
+		}
+		call_user_func_array(array($controller, $this->method), $this->args);
+	}
+}
+
