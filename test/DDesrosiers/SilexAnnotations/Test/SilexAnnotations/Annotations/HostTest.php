d6d7e64d97850e625b6f8e09061b970c91b94057
Dana Desrosiers <dana.desrosiers@gmail.com>
2014-05-04 10:38:57 -0700

    Added tests for Host annotation.

---
+++ test/DDesrosiers/SilexAnnotations/Test/SilexAnnotations/Annotations/HostTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace DDesrosiers\SilexAnnotations\Test\Annotations;
+
+use DDesrosiers\SilexAnnotations\Annotations as SLX;
+use DDesrosiers\SilexAnnotations\AnnotationServiceProvider;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Client;
+
+class HostTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Application */
+    protected $app;
+
+    /** @var Client */
+    protected $client;
+
+    public function setUp()
+    {
+        $this->app = new Application();
+        $this->app['debug'] = true;
+
+        $this->app->register(new AnnotationServiceProvider(), array(
+            "annot.srcDir" => __DIR__."/../../../../../../src",
+            "annot.controllers" => array("DDesrosiers\\SilexAnnotations\\Test\\Annotations\\HostTestController")
+        ));
+
+        $this->client = new Client($this->app, array('HTTP_HOST' => 'www.test.com'));
+    }
+
+    public function testHost()
+    {
+        $this->client->request("GET", "/rightHost");
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->client->request("GET", "/wrongHost");
+        $response = $this->client->getResponse();
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+}
+
+class HostTestController
+{
+    /**
+     * @SLX\Request(method="GET", uri="/rightHost")
+     * @SLX\Host("www.test.com")
+     */
+    public function testHost()
+    {
+        return new Response();
+    }
+
+    /**
+     * @SLX\Request(method="GET", uri="/wrongHost")
+     * @SLX\Host("www.wrong.com")
+     */
+    public function testWrongHost()
+    {
+        return new Response();
+    }
+}
