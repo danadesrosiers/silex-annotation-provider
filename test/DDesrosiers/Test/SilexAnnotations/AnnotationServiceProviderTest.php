8ebd13484b4e2a21106efafd6d444d81ea2afdb5
Dana Desrosiers <dana.desrosiers@gmail.com>
2014-06-12 22:14:10 -0700

    Add license ti test files.

--- test/DDesrosiers/Test/SilexAnnotations/AnnotationServiceProviderTest.php
+++ test/DDesrosiers/Test/SilexAnnotations/AnnotationServiceProviderTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the silex-annotation-provider package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ * @copyright (c) 2014, Dana Desrosiers <dana.desrosiers@gmail.com>
+ */
 
 namespace DDesrosiers\Test\SilexAnnotations;
 
