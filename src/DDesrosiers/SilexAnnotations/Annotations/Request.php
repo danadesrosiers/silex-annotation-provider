34a2879f507a180923a92230a8a28e3f7208003e
Dana Desrosiers <dana.desrosiers@gmail.com>
2014-06-13 20:55:53 -0700

    Update license.

--- src/DDesrosiers/SilexAnnotations/Annotations/Request.php
+++ src/DDesrosiers/SilexAnnotations/Annotations/Request.php
@@ -5,7 +5,7 @@
  * file that was distributed with this source code.
  *
  * @license    MIT License
- * @copyright (c) 2013, Dana Desrosiers <dana.desrosiers@gmail.com>
+ * @copyright (c) 2014, Dana Desrosiers <dana.desrosiers@gmail.com>
  */
 
 namespace DDesrosiers\SilexAnnotations\Annotations;
